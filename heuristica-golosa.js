801d03e7703ab17c74eb2fafb041281e0a8f8e0c
Brian Pericon <brian_pericon@itrsa.com.ar>
2022-06-30 13:57:41 -0300

    Primer aleatorizacion de eleccion del proximo

--- heuristica-golosa.js
+++ heuristica-golosa.js
@@ -13,19 +13,19 @@ export const algoritmoGoloso = (grafoCompleto, verticeInicial = 0) => {
     queue.enqueue(inicial);
     visitados[inicial] = true;
 
-    while(!queue.isEmpty()){
+    while (!queue.isEmpty()) {
         let actual = queue.dequeue(),
-            adyacentes = grafoCompleto.adyacentByNode(actual, ordernamientoPorPeso); // parte golosa: odenamiento por el mas cercano.
+            adyacentes = grafoCompleto.adyacentByNode(actual, ordernamientoPorPeso), // parte golosa: odenamiento por el mas cercano.
+            noVisitados = adyacentes.filter(aristaAdyacente => !visitados[aristaAdyacente.getNodeTo]), // filtro los adyacentes que no estan visitados
+            posicionRandom = Math.floor(Math.random() * ((noVisitados.length - 1) - 0 + 1) + 0), // elijo una posicion aleatoria
+            arista = noVisitados[posicionRandom]; // uso esa arista
 
-        for (let arista of adyacentes) {
-            if(!visitados[arista.getNodeTo]){
-                queue.enqueue(arista.getNodeTo);
-                visitados[arista.getNodeTo] = true;
-                predecesores[arista.getNodeTo] = arista.getNodeFrom;
-                resultado.push(arista);
-                break;
-            }
-        };
+        if (arista) {
+            queue.enqueue(arista.getNodeTo);
+            visitados[arista.getNodeTo] = true;
+            predecesores[arista.getNodeTo] = arista.getNodeFrom;
+            resultado.push(arista);
+        }
     }
 
     let ultimo = resultado[(grafoCompleto.numberNodes -2)];
